648c6552955457a18de612a3f67b24bfe4968902
Y4M06 <93231355+Y4M06@users.noreply.github.com.>
2026-07-03 18:23:54 +0300

    sync

--- nvim/.config/nvim/lua/Yamo/init.lua
+++ nvim/.config/nvim/lua/Yamo/init.lua
@@ -7,4 +7,16 @@ vim.cmd.colorscheme("zenblue")
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+vim.g.clipboard = {
+  name = 'wl-clipboard',
+  copy = {
+    ['+'] = 'wl-copy --foreground',
+    ['*'] = 'wl-copy --foreground --primary',
+  },
+  paste = {
+    ['+'] = 'wl-paste --no-newline',
+    ['*'] = 'wl-paste --no-newline --primary',
+  },
+  cache_enabled = true,
+}
 
